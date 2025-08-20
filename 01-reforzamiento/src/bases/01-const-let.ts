c7f86279d9a3743f01a42deb2c39586fb020a44e
Adrian IT <alexmola917@gmail.com>
2025-08-20 20:04:12 +0200

    Add const/let variable demo and import to main
    Created 01-const-let.ts to demonstrate usage of const and let variables, string methods, and reassignment. Updated main.ts to import the new module for execution.

---
+++ 01-reforzamiento/src/bases/01-const-let.ts
@@ -0,0 +1,11 @@
+
+const firstName: string = 'Adrian';
+let lastName = 'IT';
+
+console.log(firstName, lastName);
+
+const containsLetterI = lastName.includes('I');
+let diceNumber = 5;
+diceNumber = 3;
+
+console.log({containsLetterI, diceNumber});
