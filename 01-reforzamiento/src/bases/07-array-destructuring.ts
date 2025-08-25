b3036be859b0fce1390eef9c275ec91a4c36f69f
Adrian IT <alexmola917@gmail.com>
2025-08-25 17:13:19 +0200

    Add useState function example with array destructuring
    Introduced a useState function that returns a value and a setter function, demonstrating array destructuring and usage with console output.

--- 01-reforzamiento/src/bases/07-array-destructuring.ts
+++ 01-reforzamiento/src/bases/07-array-destructuring.ts
@@ -12,3 +12,15 @@ const returnsArrayFn = () => {
 const [letters, numbers] = returnsArrayFn();
 
 console.log(letters, numbers);
+
+function useState(value: string) {
+    return [
+        value,
+        (newValue: string) => {console.log(newValue);            
+        },
+    ] as const;
+};
+
+const [name, setName] = useState('Goku');
+console.log(name);       // Goku
+setName('Vegeta');       // Imprime "Vegeta"
