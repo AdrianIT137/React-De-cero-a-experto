938505f18a82d0ae68fd526b05436b24c426c606
Adrian IT <alexmola917@gmail.com>
2025-08-25 14:59:09 +0200

    Add Person and Address interfaces to object literal
    Introduced TypeScript interfaces for Person and Address to provide type safety for the ironman object. Commented out spiderman cloning and mutation code, and added a console log for ironman.

--- 01-reforzamiento/src/bases/03-object-literal.ts
+++ 01-reforzamiento/src/bases/03-object-literal.ts
@@ -1,4 +1,16 @@
-const ironman= {
+interface Person {
+    firstName: string;
+    lastName: string;
+    age: number;
+    address: Address;
+}
+
+interface Address {
+    postalCode: string;
+        city: string;
+}
+
+const ironman: Person = {
     firstName: 'Tony',
     lastName: 'Stark',
     age: 45,
@@ -8,11 +20,14 @@ const ironman= {
     },
 };
 
-const spiderman = structuredClone(ironman);
+console.log(ironman);
 
-spiderman.firstName = 'Peter';
-spiderman.lastName = 'Parker';
-spiderman.age = 22;
-spiderman.address.city = 'San José';
 
-console.log(ironman, spiderman);
+// const spiderman = structuredClone(ironman);
+
+// spiderman.firstName = 'Peter';
+// spiderman.lastName = 'Parker';
+// spiderman.age = 22;
+// spiderman.address.city = 'San José';
+
+// console.log(ironman, spiderman);
